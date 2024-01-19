d283de39ef7d03c8de04de155085750a9bcd70cf
zacker330 <zacker330@gmail.com>
2024-01-19 19:00:41 +0800

    update

--- server/src/main/java/codes/showme/server/controller/IndexController.java
+++ server/src/main/java/codes/showme/server/controller/IndexController.java
@@ -1,9 +1,8 @@
 package codes.showme.server.controller;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/")
 public class IndexController {
